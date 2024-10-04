ea68a241245090f3c70ec2881d076045caaef3ff
quessr <heosera76@gmail.com>
2024-10-04 14:23:53 +0900

    BaseActivity 구현

---
+++ app/src/main/java/yiwoo/prototype/gabobell/BaseActivity.kt
@@ -0,0 +1,26 @@
+package yiwoo.prototype.gabobell
+
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.annotation.CallSuper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<T : ViewBinding>(private val bindingFactory: (LayoutInflater) -> T) :
+    AppCompatActivity() {
+
+    private var _binding: T? = null
+    private val binding get() = _binding!!
+
+    @CallSuper
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = bindingFactory(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
