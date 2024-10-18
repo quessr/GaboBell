0bf2f2b93df2a8ee653aa7e38c743bf7632a0ed5
kimkibeom <kb@Kibeom-MacBookPro.local>
2024-10-18 13:47:04 +0900

    API 테스트용 UUID, Token 설정

--- app/src/main/java/yiwoo/prototype/gabobell/helper/UserSettingsManager.kt
+++ app/src/main/java/yiwoo/prototype/gabobell/helper/UserSettingsManager.kt
@@ -8,9 +8,9 @@ object UserSettingsManager {
     private const val KEY_EMERGENCY_FORMAT = "emergency_format"
 
     private val apiInfo: Array<Pair<String, String>> = arrayOf(
-        "JS's uuid" to "JS's token",
-        "SR's uuid" to "SR's token",
-        "KB's uuid" to "KB's token"
+        "283697cc-8e00-40da-947f-058ece8af8aa" to "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyVHlwZSI6IlVTRVIiLCJqdGkiOiJkNjM3N2VjODdmNDQ0ODBkYjhmZjI3MmVjZGJkYzQ4Nzk3NjgiLCJpc3MiOiJZSVdPTy1BUEliYzlkY2QzZWQ5Iiwic3ViIjoidXNlcjAxIiwiaWF0IjoxNzI5MjE2Njk4LCJleHAiOjE3MzE4MDg2OTh9.wx106Tadiy5tyAA3lquMHJKcJApPVSPPknKk_DnZIs0",
+        "0df40874-482b-41ee-9d4a-dcd75f1233e6" to "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyVHlwZSI6IlVTRVIiLCJqdGkiOiIzMjFiODQ4MmRmMjU0NzQ1OGQ4OTJkYmE1ZTcyMDZjMDEwMDQ2IiwiaXNzIjoiWUlXT08tQVBJNzU1NTE3N2NhYSIsInN1YiI6InVzZXIwMiIsImlhdCI6MTcyOTIxNzA1NSwiZXhwIjoxNzMxODA5MDU1fQ.a3fRtxmpc4RmU7xJO1hf4dUSoqkyjritB2f7yjqqwqc",
+        "619e4ad1-3868-47a5-9b41-b039bcac434c" to "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyVHlwZSI6IlVTRVIiLCJqdGkiOiJhODJjMWMwMjAyY2I0ZDc2ODM5MTY5MWY5ODRiOTVlMjIwNzEiLCJpc3MiOiJZSVdPTy1BUEkxMzk2MjcwNTcyIiwic3ViIjoidXNlcjAzIiwiaWF0IjoxNzI5MjE3MTkxLCJleHAiOjE3MzE4MDkxOTF9.iXt6m-3du0pnw9OvPLT_MrrwbUEoIBsNO4lDWU5a5bo"
     )
 
     enum class EmergencyFormatType(val value: Int) {
