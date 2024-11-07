13c14a1f98224e92ae309ac2cd2bee6c9f1e757d
quessr <heosera76@gmail.com>
2024-11-07 17:09:11 +0900

    토큰 존재 여부에 따라 로그인/메인 화면으로 분기 처리

--- app/src/main/java/yiwoo/prototype/gabobell/ui/SplashActivity.kt
+++ app/src/main/java/yiwoo/prototype/gabobell/ui/SplashActivity.kt
@@ -7,6 +7,7 @@ import android.os.Looper
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import yiwoo.prototype.gabobell.R
+import yiwoo.prototype.gabobell.helper.UserDataStore
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +24,17 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun moveToScreen() {
-        // TODO: 앱내 서비스 토큰 저장 여부를 체크 필요 (인증화면 or 메인화면)
-        val intent = Intent(this, MainActivity::class.java)
+        // 앱내 서비스 토큰 저장 여부를 체크 후 그에따른 화면 이동 (로그그인화면 or 메인화면)
+        val token = UserDataStore.getToken(this)
+
+        val intent: Intent = if (token == "") {
+            // 토큰 없는 경우 (회원가입을 한 적 없는 유저) -> 회원가입 화면 이동
+            Intent(this, SignInActivity::class.java)
+        } else {
+            // 토큰 있는 경우 (기존에 회원가입을 한 유저) -> 메인 화면 이동
+            // 추후 token에 유효시간이 추가되면, 로직이 추가 될 예정
+            Intent(this, MainActivity::class.java)
+        }
         startActivity(intent)
         finish()
     }
