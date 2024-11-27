fca9f65fe2efd9a9b09265af415b8fde4a4285e2
heojuseon <hjs2683@naver.com>
2024-11-27 10:38:52 +0900

    api Base_Url 주소 변경

--- app/src/main/java/yiwoo/prototype/gabobell/module/RetrofitModule.kt
+++ app/src/main/java/yiwoo/prototype/gabobell/module/RetrofitModule.kt
@@ -13,7 +13,7 @@ import yiwoo.prototype.gabobell.helper.UserDataStore
 
 object RetrofitModule {
 
-    private const val BASE_URL = "http://192.168.1.100:8080/api/v1/"
+    private const val BASE_URL = "https://ansimi.withfriends.kr:8443/api/v1/"
     private const val SEARCH_API_BASE = "https://dapi.kakao.com/"
 
     fun provideRetrofit(context: Context): Retrofit {
