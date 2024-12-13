19a0a0dfa672a2ff62a1a45782f50539aa5a817a
heojuseon <hjs2683@naver.com>
2024-12-13 09:22:47 +0900

    버전에 따른 디바이스 재부팅 대응

--- app/src/main/java/yiwoo/prototype/gabobell/ble/CommonReceiver.kt
+++ app/src/main/java/yiwoo/prototype/gabobell/ble/CommonReceiver.kt
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothAdapter
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import yiwoo.prototype.gabobell.GaboApplication
 import yiwoo.prototype.gabobell.helper.Logger
 
@@ -24,10 +25,20 @@ open class CommonReceiver: BroadcastReceiver() {
             }
         }
 
+        /**
+         * - 안드로이드 12 에서는 시스템 브로드캐스트에 반응하여 서비스를 시작하는 경우
+         *   예외적으로 startService를 허용하여 오류가 발생하지 않음
+         *   (ACTION_BOOT_COMPLETED은 허용된 브로드캐스트 중 하나)
+         * - 안드로이드 8.0 (Oreo) 이상 startForegroundService 사용
+         */
         if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
             Logger.d("ACTION_BOOT_COMPLETED")
             val serviceIntent = Intent(context, BleManager::class.java)
-            context?.startService(serviceIntent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context?.startForegroundService(serviceIntent)
+            } else {
+                context?.startService(serviceIntent)
+            }
         }
     }
 }
